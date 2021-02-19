3dd57da430330be42b3069dc0771d43941cef6dc
Benjamin Tautfest <b.tautfest@gmx.de>
2021-02-19 23:25:27 +0100

    Tray to configure allMarkdownRemark

--- gatsby.node.js
+++ gatsby.node.js
@@ -5,6 +5,38 @@ module.exports.onCreateNode = ({ node, actions }) => {
 
 	if (node.internal.type === 'MarkdownRemark') {
         const slug = path.basename(node.fileAbsolutePath, '.md')
-		console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', slug)
+        createNodeField({
+            node,
+            name: 'slug',
+            value: slug
+        })
 	}
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+    const { createPage } = actions
+    const articleTemplate = path.resolve('./src/templates/story.js')
+    const res = await graphql(`
+        query {
+            allMarkdownRemark {
+                edges {
+                    node {
+                       fields {
+                           slug
+                       } 
+                    }
+                }
+            }
+        }
+    `)
+
+    res.data.allMarkdownRemark.edges.forEach((edge) => {
+        createPage({
+            conponent: articleTemplate,
+            path: `/story/${edge.node.fields.slug}`,
+            context: {
+                slug: edge.node.fields.slug
+            }
+        })
+    })
 }
---
+++ src/templates/story.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layoutCont'
+
+const Story = () => {
+    return(
+        <Layout>
+            <div>Hallo</div>
+        </Layout>
+    )
+}
+
+export default Story
